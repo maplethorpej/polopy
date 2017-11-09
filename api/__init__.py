f7fc7b84c2b5cc1b8ebcf3c3db3e28537325a17c
maplethorpej <jeradmaplethorpe@gmail.com>
2017-11-08 20:13:14 -0600

    flask-cache implementation

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_caching import Cache
 
 app = Flask(__name__)
 CORS(app)
+
+redis_cache = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_REDIS_HOST': '127.0.0.1',
+    'CACHE_REDIS_PORT': '6379'
+}
+
+cache = Cache(config=redis_cache)
+cache.init_app(app)
+
+#with app.app_context():
+#    cache.clear()
 
 app.config['SECRET_KEY'] = b'\xc3n\xf8F7)\xf5\x97a2m\x09yx\xa5\x02\x8e~]H\x9e\x1c\xb9\xe8'
 
--- api/routes.py
+++ api/routes.py
@@ -1,9 +1,16 @@
-from api import app
-from flask import Flask
-from poloniex import Poloniex
-polo = Poloniex()
+import requests as r
+import time
+
+from api import app, cache
+from flask import Flask, jsonify
 
 
 @app.route('/', methods=['GET'])
+@cache.cached(timeout=5)
 def index():
-    return 'index'
+    # poloniex_url = 'https://poloniex.com/public'
+    # test_data = r.get(
+    #     poloniex_url + '?command=returnChartData&currencyPair=BTC_XMR&start=1405699200&end=1406700000&period=14400')
+    # print(test_data.json())
+    # return jsonify(test_data.json())
+    return time.ctime()
