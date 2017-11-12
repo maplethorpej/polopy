a638b1f7a3db38762bbbba6f6422e950411beb60
maplethorpej <jeradmaplethorpe@gmail.com>
2017-11-12 15:52:25 -0600

    error handling

--- api/routes.py
+++ api/routes.py
@@ -2,8 +2,9 @@ import requests as r
 import time
 
 from api import app, cache, poloniex_url
-from flask import jsonify, request
 from api.lib import TimeRange, range_periods
+from flask import jsonify, request
+from requests import RequestException
 
 
 @app.route('/public', methods=['GET'])
@@ -22,17 +23,32 @@ def index():
     if command == 'returnChartData':
         currencies_param = request.args.get('currencyPair')
         range_param = request.args.get('range')
-        timeout = range_periods[range_param]['expires']
+
+        try:
+            timeout = range_periods[range_param]['expires']
+        except KeyError:
+            return jsonify({'error': 'Invalid range specified.'
+                                     'Accepted: LAST_HOUR, LAST_DAY, LAST_WEEK, LAST_MONTH, LAST_YEAR, ALL_TIME'})
 
         @cache.memoize(timeout=timeout)
         def get_chart_data(currencies, time_range):
             time_period = TimeRange(time_range=time_range)
             url = poloniex_url + '?command=' + command + '&' + time_period.get_query_str + '&currencyPair=' + currencies
 
-            chart_data = r.get(url, timeout=120)
+            try:
+                chart_data = r.get(url, timeout=30)
+            except RequestException as e:
+                return jsonify({'error': str(e)})
+
             data = {'poloniex': chart_data.json()}
             return jsonify(data)
 
         return get_chart_data(currencies_param, range_param)
 
-    return time.ctime()
+    return jsonify({'error': 'Invalid or missing command parameter.'})
+
+
+@app.route('/', defaults={'path': ''}, methods=['GET'])
+@app.route('/<path:path>')
+def catch_all(path):
+    return jsonify({'error': 'Unknown endpoint.'})
